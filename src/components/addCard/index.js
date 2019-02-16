fc2fc3410e494b815d6e0131c659d9d0b8c97521
denebtech <techdeneb@gmail.com>
2019-02-15 20:08:38 -0500

    Update place holder text in expiary inputs

--- src/components/addCard/index.js
+++ src/components/addCard/index.js
@@ -24,25 +24,44 @@ const styles = StyleSheet.create({
 export default class AddCard extends Component {
   constructor(props) {
     super(props)
-    this.state = { cardNumber: '', expiryDate: '', cvc: '' }
+    this.state = { cardNumber: '', expiryYear: '', expiryMonth: '', cvc: '' }
   }
 
   render() {
     return (
       <View style={styles.test}>
         <TextInput
+          keyboardType={'numeric'}
           style={styles.textInput}
           onChangeText={(cardNumber) => this.setState({ cardNumber })}
           value={this.state.cardNumber}
           placeholder={'4242424242424242'}
         />
         <TextInput
+          maxLength={2}
+          keyboardType={'numeric'}
           style={styles.textInput}
-          onChangeText={(expiryDate) => this.setState({ expiryDate })}
-          value={this.state.expiryDate}
-          placeholder={'3/19'}
+          onChangeText={(expiryMonth) => {
+            if(expiryMonth.length === 2) {
+              this.refs.YearInput.focus()
+            }
+            this.setState({ expiryMonth })
+          }}
+          value={this.state.expiryMonth}
+          placeholder={'03'}
         />
         <TextInput
+          ref={'YearInput'}
+          maxLength={2}
+          keyboardType={'numeric'}
+          style={styles.textInput}
+          onChangeText={(expiryYear) => this.setState({ expiryYear })}
+          value={this.state.expiryYear}
+          placeholder={'19'}
+        />
+        <TextInput
+
+          keyboardType={'numeric'}
           style={styles.textInput}
           onChangeText={ (cvc) => this.setState({ cvc }) }
           value={this.state.cvc}
@@ -50,7 +69,7 @@ export default class AddCard extends Component {
         />
         <TouchableHighlight
           style={[styles.highlight, this.props.highlightStyle]}
-          onPress={() => this.props.addCardHandler(this.state.cardNumber, this.state.expiryDate, this.state.cvc)}
+          onPress={() => this.props.addCardHandler(this.state.cardNumber, this.state.expiryMonth, this.state.expiryYear, this.state.cvc)}
         >
           <Text>
             + Add Card
