00ddeb5aa84ea9f68d109c267ebd231b9f6f5764
denebtech <techdeneb@gmail.com>
2019-02-15 20:14:21 -0500

    deep merge styles for user overriding

--- src/components/selectPayment/index.js
+++ src/components/selectPayment/index.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import PaymentMethods from '../paymentMethods'
-import defaultStyles from './defaultStyle'
-import PaymentMethodContainer from '../paymentMethods/paymentMethodContainer'
+import defaultStyles from './defaultStyles'
+import TouchableOpacity from '../common/touchableOpacity'
 export default class SelectPayment extends Component {
   render() {
+    const styles = _.merge({...defaultStyles}, props.styles)
     return (
-      <View style={defaultStyles.selectPaymentContainer}>
+      <View style={styles.selectPaymentContainer}>
         <PaymentMethods
           paymentSources={this.props.paymentSources}
           selectPaymentHandler={this.props.selectPaymentHandler}
           applePayHandler={this.props.applePayHandler}
           enableApplePay={this.props.enableApplePay}
-          styles={defaultStyles}
+          styles={styles}
         />
-        <PaymentMethodContainer style={defaultStyles.addButton} styles={defaultStyles} onPress={() => this.props.addCardHandler()} last>
-          <Text style={defaultStyles.addButtonText}>Add New Card</Text>
-        </PaymentMethodContainer>
+        <TouchableOpacity style={styles.addButton} styles={styles} onPress={() => this.props.addCardHandler()} last>
+          <Text style={styles.addButtonText}>Add New Card</Text>
+        </TouchableOpacity>
       </View>
     )
   }
